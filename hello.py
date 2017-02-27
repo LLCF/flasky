5cbcf816681c2e229e8b290d8bef19e73f8ebe6c
LLCF <likai87268758@163.com>
2017-02-27 22:44:10 +0800

    v1.0.2

--- hello.py
+++ hello.py
@@ -1,13 +1,22 @@
-from flask import Flask
-
+from flask import Flask, request, abort
+from flask.ext.script import Manager
 app = Flask(__name__)
+manager = Manager(app)
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return '<h1>Hello World!</h1>', 400
 
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello, %s</h1>' % name
+@app.route('/browser')
+def browser():
+    user_agent = request.headers.get("User-Agent")
+    return "<p>Your browser is %s</p>" % user_agent
 
+@app.route("/404")
+def _404():
+    abort(404)
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    manager.run()
