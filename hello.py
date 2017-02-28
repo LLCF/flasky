2f0f3a352732c68a3c123b5d1d886cab2b44d07f
LLCF <likai87268758@163.com>
2017-02-28 09:00:03 +0800

    render_template

--- hello.py
+++ hello.py
@@ -1,18 +1,30 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort, render_template
 from flask.ext.script import Manager
 app = Flask(__name__)
 manager = Manager(app)
+# @app.route('/')
+# def index():
+#     return '<h1>Hello World!</h1>', 400
+
+
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>', 400
+    return render_template('index.html')
+# @app.route('/user/<name>')
+# def user(name):
+#     return '<h1>Hello, %s</h1>' % name
+
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s</h1>' % name
+    return render_template('user.html', name = name)
+
+
 @app.route('/browser')
 def browser():
     user_agent = request.headers.get("User-Agent")
     return "<p>Your browser is %s</p>" % user_agent
+
 
 @app.route("/404")
 def _404():
